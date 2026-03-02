6bd84ae8ce68cad98d2261cc397c7a569f2fca7d
khanhpear2004 <khanh.le31072004@hcmut.edu.vn>
2026-03-02 17:32:43 +0700

    push prefab

---
+++ Assets/UI/Scripts/Model/ChangePasswordModels.cs
@@ -0,0 +1,14 @@
+using System;
+
+[Serializable]
+public class ChangePassReq
+{
+    public string email;
+    public string oldPassword;
+    public string newPassword;
+    public string OTP;
+}
+
+
+
+
